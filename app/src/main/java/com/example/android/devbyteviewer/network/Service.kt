25b2a3085ec1419545de85efaf082716e68198e7
Filip Sedivy <mail@filipsedivy.cz>
2022-12-11 14:31:46 +0100

    Create network data transfer object and services

---
+++ app/src/main/java/com/example/android/devbyteviewer/network/Service.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.devbyteviewer.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+interface DevbyteService {
+    @GET("devbytes")
+    suspend fun getPlaylist(): NetworkVideoContainer
+}
+
+object DevByteNetwork {
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://android-kotlin-fun-mars-server.appspot.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+    val devbytes = retrofit.create(DevbyteService::class.java)
+
+}
+
+
